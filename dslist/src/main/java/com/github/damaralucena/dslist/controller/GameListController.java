6d1c35495174502d914dde6b583299851953fb23
damaralucena <damara.ti.dev@gmail.com>
2023-11-19 19:08:53 -0300

    Add move games on list

--- dslist/src/main/java/com/github/damaralucena/dslist/controller/GameListController.java
+++ dslist/src/main/java/com/github/damaralucena/dslist/controller/GameListController.java
@@ -3,15 +3,13 @@ package com.github.damaralucena.dslist.controller;
 
 import com.github.damaralucena.dslist.dto.GameListDTO;
 import com.github.damaralucena.dslist.dto.GameMinDTO;
+import com.github.damaralucena.dslist.dto.ReplacementDTO;
 import com.github.damaralucena.dslist.services.GameListService;
 import com.github.damaralucena.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,7 +28,6 @@ public class GameListController {
     return result;
 
   }
-
   @GetMapping
   public List<GameListDTO> findAll(){
     List<GameListDTO> result = gameListService.findAll();
@@ -42,5 +39,12 @@ public class GameListController {
     List<GameMinDTO> result = gameService.findByList(listId);
     return result;
   }
+
+  @PostMapping(value = "/{listId}/replacement")
+  public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+    gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+  }
+
+
 
 }
--- dslist/src/main/java/com/github/damaralucena/dslist/services/GameListService.java
+++ dslist/src/main/java/com/github/damaralucena/dslist/services/GameListService.java
@@ -3,7 +3,9 @@ package com.github.damaralucena.dslist.services;
 import com.github.damaralucena.dslist.dto.GameListDTO;
 import com.github.damaralucena.dslist.entities.Game;
 import com.github.damaralucena.dslist.entities.GameList;
+import com.github.damaralucena.dslist.projections.GameMinProjection;
 import com.github.damaralucena.dslist.repository.GameListRepository;
+import com.github.damaralucena.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +14,9 @@ import java.util.List;
 
 @Service
 public class GameListService {
+
+  @Autowired
+  public GameRepository gameRepository;
   @Autowired
   public GameListRepository gameListRepository;
 
@@ -25,6 +30,22 @@ public class GameListService {
   public List<GameListDTO> findAll(){
     List<GameList> result = gameListRepository.findAll();
     return result.stream().map(x -> new GameListDTO(x)).toList();
+  }
+
+  @Transactional
+  public void move(Long listId, int sourceIndex, int destinationIndex) {
+    List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+    GameMinProjection obj = list.remove(sourceIndex);
+    list.add(destinationIndex, obj);
+
+    int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+    int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+    for (int i = min; i <= max; i++){
+      gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+    }
+
   }
 
 }
